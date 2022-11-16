3c5c705c20702402749b20b3ee8efce84cdf3d1c
Artem <art.saikin@gmail.com>
2022-11-16 11:45:08 +0300

    Testing CL0010

---
+++ src/CatenaLogic.Analyzers.Tests/CL0010/CL0010DiagnosticFacts.cs
@@ -0,0 +1,63 @@
+﻿namespace CatenaLogic.Analyzers.Tests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class CL0010DiagnosticFacts
+    {
+        public class Reports_Diagnostic
+        {
+            private static readonly ExpectedDiagnostic ExpectedDiagnostic = ExpectedDiagnostic.Create(Descriptors.CL0010_DontKeepClassMemberRegions);
+
+            [TestCase]
+            public void InvalidCode_Contains_MembersRegions()
+            {
+                var before = @"
+namespace ConsoleApp1
+{
+    using System;
+
+    internal class Program
+    {
+        #region Fields
+        private readonly string foo1 = "";
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructors
+        public Program(object arg)
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public string Error { get; set; }
+        #endregion
+
+        #region Methods
+        private void Write(string message = "")
+        {
+            Console.WriteLine("");
+        }
+        #endregion
+    }
+}";
+
+                Solution.Verify<ClassesAnalyzer>(analyzer => RoslynAssert.Diagnostics(analyzer, ExpectedDiagnostic, before));
+            }
+        }
+
+        public class Reposts_NoDiagnostic
+        {
+            [TestCase]
+            public void ValidCode_Contains_CustomRegions()
+            {
+
+            }
+        }
+    }
+}
